ea27f89e541ecce0e8faff43329a91cdb950fb0f
Alex Tilatti <alex.tilatti.relay@gmail.com>
2025-12-04 13:08:54 -0500

    add a utilities file for validation and data transformation

---
+++ app/components/contacts/utils.ts
@@ -0,0 +1,53 @@
+import { Contact } from "../../api/api";
+import { ContactsFormInput } from "./types";
+
+const getIsURL = (text: string) => {
+  if (!text) {
+    return false;
+  }
+  return URL.canParse(text);
+};
+
+const getIsValidNumber = (data: string) => {
+  if (!data) {
+    return false;
+  }
+
+  const number = Number(data);
+
+  return number >= 0 || "Number must not be negative";
+};
+
+const transformFormData = (formData: ContactsFormInput) => {
+  if (!formData) {
+    return;
+  }
+
+  const data = {} as Record<string, unknown>;
+
+  Object.entries(formData).forEach(([key, value]) => {
+    if (key === "zipCode" && value !== null && typeof value === "string") {
+      data["zipcode"] = value;
+      return
+    }
+
+    if (
+      key === "lastContacted" &&
+      value !== null &&
+      typeof value === "string"
+    ) {
+      data["last_contacted"] = value;
+      return
+    }
+
+    if (key === "salesRep" && value !== null && typeof value === "string") {
+      data["sales_rep"] = value;
+      return
+    }
+
+    data[key] = value;
+  });
+
+  return data as Contact;
+};
+export { getIsURL, getIsValidNumber, transformFormData };
