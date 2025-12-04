3d17ae842bf55ec9cbea0c9bc3534692ed31f248
Alex Tilatti <alex.tilatti.relay@gmail.com>
2025-12-04 13:08:34 -0500

    add a new page for contacts

---
+++ app/contacts/page.tsx
@@ -0,0 +1,13 @@
+import ContactsContainer from "../components/contacts/ContactsContainer";
+
+const ContactsPage = () => {
+  return (
+    <main className="min-h-screen items-center justify-center bg-white px-4 py-10">
+      <div>
+        <ContactsContainer />
+      </div>
+    </main>
+  );
+};
+
+export default ContactsPage;
