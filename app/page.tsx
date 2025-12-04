2b1cb1b09e4b9601cfba86046f0b32c0ba2f8ecc
Alex Tilatti <alex.tilatti.relay@gmail.com>
2025-12-04 13:06:05 -0500

    refine the homepage and add the contacts context wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,26 @@
 "use client";
 import CryptoPrice from "./components/charts/Price";
 import VolumePriceCorrelation from "./components/charts/VolumePriceCorrelation";
+import ContactsContainer from "./components/contacts/ContactsContainer";
 import ContactsTable from "./components/contacts/ContactsTable";
+import { useContactsContext } from "./ContactsContext";
 
 export default function Home() {
+  const { contacts, isLoading, isError } = useContactsContext();
   return (
-    <main className="min-h-screen items-center justify-center bg-white px-4 py-24">
-      <div className="flex w-full flex-col gap-4 lg:flex-row">
-        <div className="flex-1">
-          <CryptoPrice />
-        </div>
-        <div className="flex-1">
-          <VolumePriceCorrelation />
-        </div>
+    <main className="flex flex-col min-h-screen gap-4 bg-white px-4 py-6">
+        <h1>
+          <b>Bitcoin Price and Volatility Charts</b>
+        </h1>
+      <div className="mb-5 flex w-full flex-col gap-4 lg:flex-row">
+        <CryptoPrice />
+        <VolumePriceCorrelation />
       </div>
-      <div>
-        <ContactsTable />
-      </div>
+        <ContactsTable
+          contacts={contacts}
+          isLoading={isLoading}
+          isError={isError}
+        />
     </main>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -3,12 +3,15 @@
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createTheme, ThemeProvider } from "flowbite-react";
+import ContactsContextProvider from "./ContactsContext";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ContactsContextProvider>{children}</ContactsContextProvider>
+    </QueryClientProvider>
   );
 };
 
